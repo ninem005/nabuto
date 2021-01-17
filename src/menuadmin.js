75b13fa6d8b06c605ffe6cdb574133e6d4306ab0
ninem005 <76542279+ninem005@users.noreply.github.com>
2021-01-16 20:19:16 -0400

    Add files via upload

---
+++ src/menuadmin.js
@@ -0,0 +1,35 @@
+const menuadmin = (prefix, pushname) => {
+ return `oiin aqui e o menu dos admins
+ _obs para o bot executar esses comandos\ne nescessario dar adm para o bot
+ 
+ ◪ *COMANDO DOS ADMINS*
+  │
+  ├─ ❏ ${prefix}opengc
+  ├─ ❏ ${prefix}closegc
+  ├─ ❏ ${prefix}promote @da pessoa (promover alguém para adm)
+  ├─ ❏ ${prefix}demote @
+(tira o adm da pessoa)
+
+  ├─ ❏ ${prefix}tagall
+(marcar todas pessoas)
+  ├─ ❏ ${prefix}tagall2
+  ├─ ❏ ${prefix}tagall3
+  ├─ ❏ ${prefix}tagall4
+  ├─ ❏ ${prefix}tagall5
+  ├─ ❏ ${prefix}add
+(add uma pessoa no gp)
+  ├─ ❏ ${prefix}kick @
+(remover uma Pessoa)
+  ├─ ❏ ${prefix}listadmins
+(lista de admins)
+  ├─ ❏ ${prefix}linkgroup
+  ├─ ❏ ${prefix}leave
+  ├─ ❏ ${prefix}welcome
+recomendo ativar(da boas vindas)
+  └─ ❏ ${prefix}ownergroup
+ `
+
+
+}
+
+exports.menuadmin = menuadmin
